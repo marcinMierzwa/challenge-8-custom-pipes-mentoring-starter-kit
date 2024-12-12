3e11c68d24c2be4c1a0b811c2f5a4203c7dd530d
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-12-12 12:44:40 +0100

    update

--- challenge-8/src/app/Components/search-bar/search-bar.component.ts
+++ challenge-8/src/app/Components/search-bar/search-bar.component.ts
@@ -1,25 +1,27 @@
-import { Component,  input, InputSignal, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component,  inject,  input, InputSignal, output } from '@angular/core';
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe } from '../../Pipes/translate.pipe';
 import { Language } from '../../Models/language.enum';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [FormsModule, TranslatePipe],
+  imports: [ReactiveFormsModule, TranslatePipe],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent  {
 
-  searchBarControl = '';
+  formBuilder: NonNullableFormBuilder = inject(NonNullableFormBuilder);
+  searchBar: FormControl = this.formBuilder.control("");
+
 
   language: InputSignal<Language> = input.required<Language>();
   SearchBarValueChange = output<string>();
 
-  onInputChange(inputValue: string): void {
+  onInputChange() {
+    const inputValue = this.searchBar.getRawValue();
     this.SearchBarValueChange.emit(inputValue);
-    
   }
 
 }
--- challenge-8/src/app/app.component.ts
+++ challenge-8/src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component, effect, inject, signal, Signal, WritableSignal } from '@angular/core';
+import { Component, inject, signal, Signal, WritableSignal } from '@angular/core';
 import { ProductService } from './Services/product.service';
 import { ProductInterface } from './Models/product-interface';
 import { toSignal } from '@angular/core/rxjs-interop'
-import {CdkTableModule} from '@angular/cdk/table';
-import { TruncatePipe } from './Pipes/truncate.pipe';
-import { FallbackImagePipe } from './Pipes/fallback-image.pipe';
-import { FormatPricePipe } from './Pipes/format-price.pipe';
-import { PublishedSincePipe } from './Pipes/published-since.pipe';
-import { TimePeriod } from './Models/time-period.enum';
-import { FormatProductCodePipe } from './Pipes/format-product-code.pipe';
 import { Language } from './Models/language.enum';
-import { TranslatePipe } from './Pipes/translate.pipe';
 import { SearchBarComponent } from './Components/search-bar/search-bar.component';
+import { TableComponent } from "./Components/table/table.component";
 import { TRANSLATIONS } from './translations';
-import { SearchPipe } from './Pipes/search.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ SearchBarComponent ,CdkTableModule, TruncatePipe, FallbackImagePipe, FormatPricePipe, PublishedSincePipe, FormatProductCodePipe, TranslatePipe, SearchPipe],
+  imports: [SearchBarComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [
@@ -27,23 +19,16 @@ import { SearchPipe } from './Pipes/search.pipe';
       useValue: TRANSLATIONS
     },
   ]
+
 })
 export class AppComponent {
   title = 'mentoring-program-starter-kit';
-  timePeriod: TimePeriod = TimePeriod.MONTHS;
   language: WritableSignal<Language> = signal<Language>(Language.EN);
 
   productService: ProductService = inject(ProductService);
 
   readonly productsSignal: Signal <ProductInterface[]> = toSignal(this.productService.getProducts(), {initialValue: []});
   searchBarSignal: WritableSignal<string> = signal<string>(''); 
-
-  displayedColumns: string[] = ['name', 'imageUrl', 'price', 'publishedAt', 'id']
-  dataSource: ProductInterface[] = this.productsSignal();
-
-  constructor() {
-    effect(() =>  this.dataSource = this.productsSignal());
-  }  
 
   onLanguageChange(language: 'EN' | 'PL'): void {
     language === 'EN' ? this.language.set(Language.EN) : this.language.set(Language.PL);
